b1792bba5271ee9eca77b47c0d6dc8b063e3c6f9
ZahidSmile <zahid.gaditek@gmail.com>
2022-09-03 02:48:37 +0500

    Created Register Flow Files

--- modules/Register/Register.py
+++ modules/Register/Register.py
@@ -1,6 +1,7 @@
 from Main import Main
 from page_objects.LoginPage import LoginPage
 from page_objects.RegisterPage import RegisterPage
+from selenium.webdriver.common.keys import Keys
 import os
 import time
 
@@ -27,15 +28,166 @@ class Register:
             action.password().send_keys(os.getenv("PASSWORD"))
             time.sleep(1)
             action.login_button().click()
+            time.sleep(3)
 
         except Exception as e:
             Main.error(str(e))
 
     def steps(self):
+        self.driver.implicitly_wait(10)
         action = RegisterPage(self.driver)
+
+        step_1 = action.step_1()
+        self.driver.execute_script("return arguments[0].scrollIntoView(true);", step_1)
+        time.sleep(2)
+        action.step_1().click()
+        time.sleep(3)
+
         Main.info("Clicking on Start Button")
         action.start().click()
 
+        time.sleep(2)
+        Main.info("Clicking on Upload Profile Field")
+        try:
+            if action.profile_exist():
+                action.profile_exist().click()
+        except Exception as e:
+            Main.error(str(e))
+        time.sleep(3)
+        action.profile().send_keys(os.getcwd() + '\image.jpg')
+
+        time.sleep(2)
+        Main.info("Clicking on Country Field")
+        action.country().click()
+        time.sleep(1)
+        action.country().send_keys('Pakistan')
+        action.country().send_keys(Keys.RETURN)
+
+        time.sleep(2)
+        Main.info("Clicking on State Field")
+        action.state().click()
+        time.sleep(1)
+        action.state().send_keys('Sindh')
+        action.state().send_keys(Keys.RETURN)
+
+        time.sleep(2)
+        Main.info("Clicking on City Field")
+        action.city().click()
+        time.sleep(1)
+        action.city().send_keys('Karachi')
+        action.city().send_keys(Keys.RETURN)
+
+        time.sleep(2)
+        Main.info("Clicking on Address Field")
+        action.address().clear()
+        time.sleep(2)
+        action.address().click()
+        action.address().send_keys('PECHS')
+
+        section = action.step_3()
+        self.driver.execute_script("return arguments[0].scrollIntoView(true);", section)
+
+        time.sleep(2)
+        Main.info("Clicking on Gender Field")
+        action.gender().click()
+
+        time.sleep(2)
+        Main.info("Clicking on Language Field")
+        try:
+            if action.ex_lang():
+                action.ex_lang().click()
+        except Exception as e:
+            Main.error(str(e))
+        time.sleep(2)
+        action.language().click()
+        action.language().send_keys('Urdu')
+        action.language().send_keys(Keys.RETURN)
+
+        time.sleep(2)
+        Main.info("Clicking on About Me Field")
+        action.about().clear()
+        time.sleep(2)
+        action.about().click()
+        action.about().send_keys("There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...")
+
+        time.sleep(2)
+        Main.info("Clicking on Save & Proceed to Step 3")
+        action.step_2().click()
+
+        self.driver.implicitly_wait(10)
+        Main.info("Clicking on Qualification Field")
+        try:
+            if action.ex_qualf():
+                action.ex_qualf().click()
+        except Exception as e:
+            Main.error(str(e))
+        time.sleep(2)
+        action.qualifications().click()
+        action.qualifications().send_keys('Other')
+        action.qualifications().send_keys(Keys.RETURN)
+
+        time.sleep(3)
+        Main.info("Clicking on Add Photo Field")
+        try:
+            if action.certificate_exist():
+                action.certificate_exist().click()
+        except Exception as e:
+            Main.error(str(e))
+        time.sleep(2)
+        action.certificate().send_keys(os.getcwd() + '\certificate.jpg')
+
+        time.sleep(3)
+        Main.info("Clicking on Experience Title")
+        action.ex_title().clear()
+        time.sleep(2)
+        action.ex_title().click()
+        action.ex_title().send_keys('Senior Qari')
+
+        time.sleep(3)
+        Main.info("Clicking on Start Date")
+        action.from_date().click()
+        action.from_date().send_keys(Keys.ARROW_LEFT)
+        action.from_date().send_keys(Keys.ARROW_LEFT)
+        action.from_date().send_keys('10')
+        action.from_date().send_keys('09')
+        action.from_date().send_keys('10092006')
+
+        time.sleep(3)
+        Main.info("Clicking on End Date")
+        action.to_date().click()
+        action.from_date().send_keys(Keys.ARROW_LEFT)
+        action.from_date().send_keys(Keys.ARROW_LEFT)
+        action.to_date().send_keys('10')
+        action.to_date().send_keys('09')
+        action.to_date().send_keys('2010')
+
+        time.sleep(3)
+        Main.info("Clicking on End Date")
+        action.exp_details().clear()
+        time.sleep(2)
+        action.exp_details().click()
+        action.exp_details().send_keys('Share some more details about your roles and responsibilities in this job')
+
+        time.sleep(3)
+        Main.info("Clicking on Save & Proceed Step 4")
+        action.step_3().click()
+
+        step_4 = action.step_4()
+        self.driver.execute_script("return arguments[0].scrollIntoView(true);", step_4)
+
+        self.driver.implicitly_wait(10)
+        Main.info("Clicking on Video Link 1")
+        action.video_1().clear()
+        time.sleep(2)
+        action.video_1().click()
+        action.video_1().send_keys('https://youtu.be/qaritester')
+
+        time.sleep(3)
+        Main.info("Clicking on Video Link 2")
+        action.video_2().clear()
+        time.sleep(2)
+        action.video_2().click()
+        action.video_2().send_keys('https://youtu.be/qaritester')
 
 
 case = Register
--- page_objects/RegisterPage.py
+++ page_objects/RegisterPage.py
@@ -8,14 +8,77 @@ class RegisterPage:
     def start(self):
         return self.driver.find_element(By.CSS_SELECTOR, ".FillingButton button")
 
-    def password(self):
-        return self.driver.find_element(By.CSS_SELECTOR, "#Password")
+    def profile(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '#profilepicture')
 
-    def login_button(self):
-        return self.driver.find_element(By.CSS_SELECTOR, 'body app-template button.btn-primary')
+    def profile_exist(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '.ProfileImage span.cancel')
 
-    def error_message(self):
-        return self.driver.find_elements(By.CSS_SELECTOR, 'body .invalid-feedback.ng-star-inserted')
+    def country(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "#Country")
+
+    def state(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '#stateId')
+
+    def city(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '#cityId')
+
+    def address(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '#Address')
+
+    def gender(self):
+        return self.driver.find_element(By.CSS_SELECTOR, 'input[value="male"]')
+
+    def language(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '#cdk-step-content-0-1 ng-select div.ng-input input[type=text]')
+
+    def ex_lang(self):
+        return self.driver.find_elements(By.CSS_SELECTOR, 'span.ng-value-icon')[0]
+
+    def about(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '#About')
+
+    def step_2(self):
+        return self.driver.find_elements(By.CSS_SELECTOR, 'button span.ng-star-inserted')[0]
+
+    def qualifications(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '#cdk-step-content-0-2 div.ng-input > input[type=text]')
+
+    def ex_qualf(self):
+        return self.driver.find_elements(By.CSS_SELECTOR, 'span.ng-value-icon')[1]
+
+    def certificate(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '#item_images')
+
+    def certificate_exist(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '.ImageList span.cancel')
+
+    def ex_title(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '.ExperiecesDetails [formcontrolname="title"]')
+
+    def from_date(self):
+        return self.driver.find_element(By.CSS_SELECTOR, 'div.Dates [placeholder="From"]')
+
+    def to_date(self):
+        return self.driver.find_element(By.CSS_SELECTOR, 'div.Dates [placeholder="to"]')
+
+    def exp_details(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '#exp_details')
+
+    def video_1(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '#video1')
+
+    def video_2(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '#video2')
+
+    def step_1(self):
+        return self.driver.find_elements(By.CSS_SELECTOR, '.tab')[0]
+
+    def step_3(self):
+        return self.driver.find_elements(By.CSS_SELECTOR, 'button span.ng-star-inserted')[1]
+
+    def step_4(self):
+        return self.driver.find_elements(By.CSS_SELECTOR, 'button span.ng-star-inserted')[2]
 
 
 
