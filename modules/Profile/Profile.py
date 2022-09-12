e321b979c948f17afb3c7f5ef2c7b59bf12780f7
ZahidSmile <zahid.gaditek@gmail.com>
2022-09-12 22:28:41 +0500

    Updated Profile.py For Negative Cases

--- modules/Profile/Profile.py
+++ modules/Profile/Profile.py
@@ -12,6 +12,8 @@ class Profile:
 
         self.login()
         self.profile()
+        self.profile_neg1()
+        self.profile_neg2()
 
     def login(self):
         Main.info("Performing Login")
@@ -130,5 +132,193 @@ class Profile:
         action.billing().click()
         action.billing().send_keys("PECHS Block 2 Karachi")
 
+    def profile_neg1(self):
+        self.driver.refresh()
+        self.driver.implicitly_wait(10)
+        action = ProfilePage(self.driver)
+
+        time.sleep(2)
+        Main.info("Selecting Radio Button I want to Teach")
+        action.teach().click()
+        action.charges_pm().click()
+
+        time.sleep(2)
+        scroll = action.add_timing()
+        self.driver.execute_script("return arguments[0].scrollIntoView(true);", scroll)
+
+        time.sleep(2)
+        Main.info("Selecting Class Per Week = 2")
+        action.class_pw().click()
+
+        time.sleep(2)
+        Main.info("Selecting Class Charges/Month = 30")
+        action.charges_pm().click()
+        time.sleep(1)
+        action.charges_pm().click()
+        time.sleep(1)
+        action.charges_pm().click()
+        time.sleep(1)
+        action.charges_pm().click()
+        time.sleep(1)
+
+        time.sleep(2)
+        Main.info("Selecting Class Duration/Minute = 45")
+        action.class_duration().click()
+        time.sleep(1)
+        action.class_duration().click()
+
+        time.sleep(2)
+        Main.info("Selecting Class Covered/Subjects = 45")
+        action.subjects().click()
+        time.sleep(1)
+        action.subjects().send_keys("hifz")
+        time.sleep(1)
+        action.subjects().send_keys(Keys.RETURN)
+
+
+        time.sleep(2)
+        Main.info("Adding New Package")
+        action.add_package().click()
+
+        time.sleep(2)
+        scroll = action.day_7()
+        self.driver.execute_script("return arguments[0].scrollIntoView(true);", scroll)
+
+        time.sleep(2)
+        Main.info("Adding Time Slot")
+        action.add_timing().click()
+
+        time.sleep(2)
+        Main.info("Removing Time Slot")
+        action.remove_timing().click()
+
+        time.sleep(2)
+        Main.info("Adding Day 2 as Monday")
+        action.day_2().click()
+
+
+        time.sleep(2)
+        scroll = action.submit()
+        self.driver.execute_script("return arguments[0].scrollIntoView(true);", scroll)
+
+        time.sleep(2)
+        Main.info("Selecting Class Duration = 45 Mins")
+        time.sleep(1)
+        action.cls_duration().click()
+        time.sleep(1)
+        action.cls_duration().click()
+
+        time.sleep(2)
+        Main.info("Providing Value for IBAN Number")
+        action.iban().click()
+        action.iban().send_keys("0000864890")
+
+        time.sleep(2)
+        Main.info("Providing Value for Account Number")
+        action.account().click()
+        action.account().send_keys("Qari Tester")
+
+        time.sleep(2)
+        Main.info("Providing Value for Billing Address")
+        action.billing().click()
+        action.billing().send_keys("PECHS Block 2 Karachi")
+
+        time.sleep(2)
+        Main.info("Performing Click On Submit Button")
+        action.submit().click()
+
+    def profile_neg2(self):
+        self.driver.refresh()
+        self.driver.implicitly_wait(10)
+        action = ProfilePage(self.driver)
+
+        time.sleep(2)
+        Main.info("Selecting Radio Button I want to Teach")
+        action.teach().click()
+        action.charges_pm().click()
+
+        time.sleep(2)
+        scroll = action.add_timing()
+        self.driver.execute_script("return arguments[0].scrollIntoView(true);", scroll)
+
+        time.sleep(2)
+        Main.info("Selecting Class Per Week = 2")
+        action.class_pw().click()
+
+        time.sleep(2)
+        Main.info("Selecting Class Charges/Month = 30")
+        action.charges_pm().click()
+        time.sleep(1)
+        action.charges_pm().click()
+        time.sleep(1)
+        action.charges_pm().click()
+        time.sleep(1)
+        action.charges_pm().click()
+        time.sleep(1)
+
+        time.sleep(2)
+        Main.info("Selecting Class Duration/Minute = 45")
+        action.class_duration().click()
+        time.sleep(1)
+        action.class_duration().click()
+
+        time.sleep(2)
+        Main.info("Selecting Class Covered/Subjects = 45")
+        action.subjects().click()
+        time.sleep(1)
+        action.subjects().send_keys("hifz")
+        time.sleep(1)
+        action.subjects().send_keys(Keys.RETURN)
+
+
+        time.sleep(2)
+        Main.info("Adding New Package")
+        action.add_package().click()
+
+        time.sleep(2)
+        Main.info("Removing New Package")
+        action.remove_package().click()
+
+        time.sleep(2)
+        scroll = action.day_7()
+        self.driver.execute_script("return arguments[0].scrollIntoView(true);", scroll)
+
+        time.sleep(2)
+        Main.info("Adding Time Slot")
+        action.add_timing().click()
+
+        time.sleep(2)
+        Main.info("Removing Time Slot")
+        action.remove_timing().click()
+
+        time.sleep(2)
+        scroll = action.submit()
+        self.driver.execute_script("return arguments[0].scrollIntoView(true);", scroll)
+
+        time.sleep(2)
+        Main.info("Selecting Class Duration = 45 Mins")
+        time.sleep(1)
+        action.cls_duration().click()
+        time.sleep(1)
+        action.cls_duration().click()
+
+        time.sleep(2)
+        Main.info("Providing Value for IBAN Number")
+        action.iban().click()
+        action.iban().send_keys("0000864890")
+
+        time.sleep(2)
+        Main.info("Providing Value for Account Number")
+        action.account().click()
+        action.account().send_keys("Qari Tester")
+
+        time.sleep(2)
+        Main.info("Providing Value for Billing Address")
+        action.billing().click()
+        action.billing().send_keys("PECHS Block 2 Karachi")
+
+        time.sleep(2)
+        Main.info("Performing Click On Submit Button")
+        action.submit().click()
 
 case = Profile
